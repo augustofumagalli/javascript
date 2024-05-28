90c5f540817851d923e070d2457d878d0355f660
Augusto Fumagalli Bonifacio Pereira <augusto.fumagalli8@gmail.com>
2024-05-28 20:33:52 -0300

    Atualizando dia 2

---
+++ script.js
@@ -0,0 +1,15 @@
+// Esta é a parte A do programa
+
+console.log('Augusto')
+console.warn('Erro 300')
+console.error('Erro 550')
+
+// Esta é a parte B do programa
+console.group('App')
+console.log('Augusto')
+console.warn('Erro 300')
+console.error('Erro 550')
+console.groupEnd()
+console.groupEnd()
+console.groupEnd()
+console.groupEnd()
